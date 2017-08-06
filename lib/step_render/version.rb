f6074ad2aca8188f9037620c9161788ee40edaad
石原将 <eastresident@ishiharasusumunoMacBook-Pro.local>
2017-08-06 23:02:27 +0900

    no longer nullstore case

--- lib/step_render/version.rb
+++ lib/step_render/version.rb
@@ -1,3 +1,3 @@
 module StepRender
-  VERSION = '0.1.0'
+  VERSION = '0.1.1'
 end
--- lib/step_render/view_helper.rb
+++ lib/step_render/view_helper.rb
@@ -2,15 +2,11 @@
 module StepRender
   module ViewHelper
     def step_render(*options)
-      if Rails.cache.is_a?(ActiveSupport::Cache::NullStore)
-        render *options
-      else
-        key = "lazy-partial-#{SecureRandom.hex(12)}"
-        cookies.signed[key] = { value: Marshal.dump(ActiveJob::Arguments.serialize(options)), expires: 2.hour.from_now }
-        # if use cache store
-        # Rails.cache.fetch(key, skip_digest: true, expires_in: 10.minute) { ActiveJob::Arguments.serialize(options) }
-        %(<div class= "related-articles lazyload" data-include="#{step_render_path(key)}">loading</div>).html_safe
-      end
+      key = "lazy-partial-#{SecureRandom.hex(12)}"
+      cookies.signed[key] = { value: Marshal.dump(ActiveJob::Arguments.serialize(options)), expires: 2.hour.from_now }
+      # if use cache store
+      # Rails.cache.fetch(key, skip_digest: true, expires_in: 10.minute) { ActiveJob::Arguments.serialize(options) }
+      %(<div class= "related-articles lazyload" data-include="#{step_render_path(key)}">loading</div>).html_safe
     end
     def import_step_render
       '<script src="https://cdnjs.cloudflare.com/ajax/libs/lazysizes/3.0.0/lazysizes.min.js"></script><script src="https://cdnjs.cloudflare.com/ajax/libs/lazysizes/3.0.0/plugins/include/ls.include.min.js"></script>'.html_safe
